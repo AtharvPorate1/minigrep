b3a5f6cbe9048434b8f037fbbfba9447bdabf09e
[AtharvPorate1] <www.atharvporate254@gmail.com>
2024-05-15 08:58:15 +0530

    First implementaion

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 use std::env::args;
+use std::fs;
 
 
 fn main() {
 
-    let filename = args().nth(1).expect("Please provide a filename");
+    let search = args().nth(1).expect("Please provide a filename");
     let path = args().nth(2).expect("Please provide a path");
     
     let args = Cli{
-        filename,
+        search,
         path: std::path::PathBuf::from(path),
     };
     
-    println!("filename: {:?}, path: {:?}", args.filename, args.path);
+    println!("filename: {:?}, path: {:?}", args.search, args.path);
+
+    let content = fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines(){
+        if line.contains(&args.search){
+            println!("{}", line);
+        }
+    
+    }
+
+
+
 }
 
 struct Cli{
-    filename: String,
+    search: String,
     path: std::path::PathBuf,
 }
